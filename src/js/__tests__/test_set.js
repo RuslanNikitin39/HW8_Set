3375b55dc8545c3b3774a98447a510027eb0289c
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2023-08-27 14:52:37 +0500

    build commit

---
+++ src/js/__tests__/test_set.js
@@ -0,0 +1,24 @@
+import Team from '../team';
+import Character from '../character';
+
+test('add, добавляет выбранного персонажа или сообщает об ошибке', () => {
+  const team = new Team();
+  team.add('swordsman');
+  expect(() => team.add('swordsman')).toThrow(new Error('Игрок уже в команде'));
+});
+
+test('addAll', () => {
+  const players = new Team();
+  const player1 = new Character('swordsman');
+  const player2 = new Character('bowman');
+  const player3 = new Character('magician');
+  players.addAll(player1, player2, player3);
+  expect(players.members.size).toEqual(3);
+});
+
+test('конвертация Set в массив', () => {
+  const team = new Team();
+  team.addAll('swordsman', 'magician', 'magician', 'bowman');
+  team.toArray();
+  expect(team.members).toEqual(['swordsman', 'magician', 'bowman']);
+});
